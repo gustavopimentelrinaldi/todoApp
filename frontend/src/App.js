144152bc104f6292aa31236ec54f7f3a6fcdc181
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2017-12-18 17:55:13 -0200

    Components Todo/About

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Todo from './todo/todo'
+import About from './todo/about'
+import 'react-bootstrap/dist/bootstrap.min.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default props => (
+  <div className="container">
+   <Todo/>
+   <About/>
+  </div>
+)
