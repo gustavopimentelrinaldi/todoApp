5a2a8ba529d4da4b35845f7a34c5b071d8f63a7d
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2017-12-18 18:10:56 -0200

    Menu Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Todo from './todo/todo'
 import About from './todo/about'
+import Menu from './template/menu'
 import 'react-bootstrap/dist/bootstrap.min.css';
 
 export default props => (
-  <div className="container">
-   <Todo/>
-   <About/>
+  <div>
+    <Menu/>
+    <Todo/>
+    <About/>
   </div>
 )
---
+++ frontend/src/template/menu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default props => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+                <a className="navbar-brand" href="">
+                    TodoApp
+                </a>
+
+            <div id="navbar" className="collapse navbar-collapse">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="">dsadsaa</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">adasdasd</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)
